e6bcc8549c9c1f7360f56bcf76f006d36300b415
jojuba <emprendojuarez@gmail.com>
2022-03-24 15:41:35 +0100

    fix int

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.css'
-import { getLibrary } from '../config/web3';
+import "../styles/globals.css";
+import { getLibrary } from "../config/web3";
 import { Web3ReactProvider } from "@web3-react/core";
 
 function MyApp({ Component, pageProps }) {
-  return  (
-      <Web3ReactProvider getLibrary={getLibrary}>
-              <Component {...pageProps} />
-      </Web3ReactProvider>
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
